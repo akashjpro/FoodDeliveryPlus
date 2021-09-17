8d4384141345bfa9495ecd3e357156bc263eb51e
Akashj Pro <akashjpro@gmail.com>
2021-09-17 20:35:19 +0700

    Update UI

--- lib/screens/loginAndSignUp/loginAndSignUp.dart
+++ lib/screens/loginAndSignUp/loginAndSignUp.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class LoginAndSignUp extends StatefulWidget {
   @override
@@ -13,58 +15,36 @@ class LoginAndSignUpState extends State<LoginAndSignUp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: CustomScrollView(slivers: <Widget>[
-      SliverAppBar(
-        pinned: _pinned,
-        snap: _snap,
-        floating: _floating,
-        expandedHeight: 160.0,
-        backgroundColor: Colors.white,
-        flexibleSpace: const FlexibleSpaceBar(
-          title: Text(''),
-          background: Image(image: AssetImage('images/logo_login.png')),
-        ),
-      ),
-      SliverFillRemaining(
-        child: new Container(
-          child: DefaultTabController(
-            initialIndex: 1,
-            length: 2,
-            child: Scaffold(
-              appBar: AppBar(
-                backgroundColor: Colors.white,
-                title: const Text(''),
-                bottom: const TabBar(
-                  tabs: <Widget>[
-                    Tab(
-                        icon: Text("Login",
-                            style: TextStyle(
-                                color: Colors.black,
-                                fontWeight: FontWeight.bold),
-                            textDirection: TextDirection.ltr)),
-                    Tab(
-                      icon: Text("Sign-up",
-                          style: TextStyle(
-                              color: Colors.black, fontWeight: FontWeight.bold),
-                          textDirection: TextDirection.ltr),
-                    ),
-                  ],
-                ),
-              ),
-              body: const TabBarView(
-                children: <Widget>[
-                  Center(
-                    child: Text("Login Screen"),
-                  ),
-                  Center(
-                    child: Text("Sign-up Screen"),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ),
-      )
-    ]));
+      backgroundColor: Colors.white,
+      body: Container(
+          color: Color(0xffbcbaba),
+          margin: const EdgeInsets.only(left: 0.0, right: 0.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                  decoration: new BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: new BorderRadius.only(
+                          bottomLeft: const Radius.circular(30.0),
+                          bottomRight: const Radius.circular(30.0))),
+                  child: Column(
+                      children: [
+                        Padding(
+                            padding: EdgeInsets.all(60.0),
+                            child:
+                            Image(image: AssetImage('images/logo_login.png')))
+                      ],
+                      TabBar(
+                        tabs: [
+                          Tab(icon: Icon(Icons.directions_car)),
+                          Tab(icon: Icon(Icons.directions_transit)),
+                          Tab(icon: Icon(Icons.directions_bike)),
+                        ],
+                      )
+                  )),
+            ],
+          )),
+    );
   }
 }
