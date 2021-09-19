f566a7a6568e7b3d1c867c252a0a4c6949d8b7e8
Akashj Pro <akashjpro@gmail.com>
2021-09-19 12:48:07 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_zoom_drawer/flutter_zoom_drawer.dart';
+import 'package:food_delivery/screens/home/widgets/home_bridge.dart';
 import 'package:food_delivery/screens/screens.dart';
 import 'package:food_delivery/theme.dart';
 
@@ -13,36 +14,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: theme(),
-      home: Bridge(),
+      home: HomeBridge(),
     );
   }
 }
 
-class Bridge extends StatefulWidget {
-  const Bridge({Key? key}) : super(key: key);
-
-  @override
-  _BridgeState createState() => _BridgeState();
-}
-
-class _BridgeState extends State<Bridge> {
-  final _zoomDrawerController = ZoomDrawerController();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: ZoomDrawer(
-      style: DrawerStyle.Style1,
-      controller: _zoomDrawerController,
-      mainScreen: HomeScreen(drawerController: _zoomDrawerController),
-      menuScreen: DrawerScreen(),
-      borderRadius: 30.0,
-      showShadow: true,
-      angle: 0.0,
-      backgroundColor: Colors.grey[300]!,
-      slideWidth: MediaQuery.of(context).size.width * .65,
-    )
-        // (ZoomDrawer.of(context)!.widget.isRtl ? .45 : 0.65)),
-        );
-  }
-}
---
+++ lib/screens/home/widgets/home_bridge.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_zoom_drawer/flutter_zoom_drawer.dart';
+import 'package:food_delivery/screens/drawer/drawer_screen.dart';
+
+import '../home_screen.dart';
+
+class HomeBridge extends StatefulWidget {
+  const HomeBridge({Key? key}) : super(key: key);
+
+  @override
+  _HomeBridgeState createState() => _HomeBridgeState();
+}
+
+class _HomeBridgeState extends State<HomeBridge> {
+  final _zoomDrawerController = ZoomDrawerController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: ZoomDrawer(
+          style: DrawerStyle.Style1,
+          controller: _zoomDrawerController,
+          mainScreen: HomeScreen(drawerController: _zoomDrawerController),
+          menuScreen: DrawerScreen(),
+          borderRadius: 30.0,
+          showShadow: true,
+          angle: 0.0,
+          backgroundColor: Colors.grey[300]!,
+          slideWidth: MediaQuery.of(context).size.width * .65,
+        )
+      // (ZoomDrawer.of(context)!.widget.isRtl ? .45 : 0.65)),
+    );
+  }
+}
+
